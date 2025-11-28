656e08ad2c3dcc48eff0afc212ec6b1d0805b12a
João Lucas <devjoaolsantos@gmail.com>
2025-11-28 11:09:12 -0300

    primeiro commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comnetário <b>JavaDoc</b>
+ */
+public class Aula01OlaDevDojo {
+
+// Comentário de uma linha
+
+    /*
+     * Cometário de mutiplas linhas
+     * */
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
