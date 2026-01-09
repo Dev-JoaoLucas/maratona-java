e2c331ed9d5be1e583967290d2d3c53b4a377752
João Lucas <devjoaolsantos@gmail.com>
2026-01-08 21:35:15 -0300

    Praticas da aula 11

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,14 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
+        long numeroGrabde = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
         System.out.println("A idade e "+idade+" anos");
     }
 }
